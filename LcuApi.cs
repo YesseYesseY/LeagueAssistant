19f767a4765d25f34422758a991a5a4d96dfdb44
YesseYesseY <yesseyyesyessey@gmail.com>
2024-01-08 17:23:31 +0100

    Safely exit

--- LcuApi.cs
+++ LcuApi.cs
@@ -30,11 +30,19 @@ class LcuApi
         }
     }
 
-    public async Task<string> GetIngame(string path)
+    public bool GetIngame(string path, out string content)
     {
-        var response = await httpClient.GetAsync($"https://127.0.0.1:2999/liveclientdata/{path}");
-        var content = await response.Content.ReadAsStringAsync();
-        return content;
+        try 
+        {
+            var response = httpClient.GetAsync($"https://127.0.0.1:2999/liveclientdata/{path}").Result;
+            content = response.Content.ReadAsStringAsync().Result;
+            return true;
+        }
+        catch
+        {
+            content = "";
+            return false;
+        }
     }
 
     public async Task<string> Get(string path)
@@ -44,16 +52,26 @@ class LcuApi
         return content;
     }
 
-    public async Task<List<Item>> GetItems(Player player)
+    public bool GetItems(Player player, out List<Item> items)
     {
-        var response = await GetIngame($"playeritems?summonerName={player.SummonerName}");
-        return JsonConvert.DeserializeObject<List<Item>>(response) ?? new List<Item>();
+        if (!GetIngame($"playeritems?summonerName={player.SummonerName}", out var content))
+        {
+            items = new List<Item>();
+            return false;
+        }
+        items = JsonConvert.DeserializeObject<List<Item>>(content) ?? new List<Item>();
+        return true;
     }
     
-    public async Task<List<Player>> GetPlayerList()
+    public bool GetPlayerList(out List<Player> playerlist)
     {
-        var response = await GetIngame("playerlist");
-        return JsonConvert.DeserializeObject<List<Player>>(response) ?? new List<Player>();
+        if(!GetIngame("playerlist", out var content))
+        {
+            playerlist = new List<Player>();
+            return false;
+        }
+        playerlist = JsonConvert.DeserializeObject<List<Player>>(content) ?? new List<Player>();
+        return true;
     }
 
     public async Task SetupItems()
--- Program.cs
+++ Program.cs
@@ -44,114 +44,121 @@ await lcuApi.SetupItems();
 Console.Clear();
 Console.WriteLine(basicLayout);
 
+await StartThing();
 
-var playerList = await lcuApi.GetPlayerList();
-while (playerList.Count <= 0)
+void Exit(string reason = "")
 {
-    playerList = await lcuApi.GetPlayerList();
-    await Task.Delay(1000);
-}
-foreach (var player in playerList)
-{
-    Console.WriteLine($"Player: {player.SummonerName} - {player.Position} - {player.Team}");
+    Console.Clear();
+    Console.WriteLine(reason);
+    Environment.Exit(0);
 }
 
-Console.SetCursorPosition(1, 1);
-Console.Write("ORDER");
-
-Console.SetCursorPosition(33, 1);
-Console.Write("CHAOS");
-Dictionary<string, List<int>> teamPositions = new Dictionary<string, List<int>>();
-foreach (var player in playerList)
-{
-    switch (player.Position)
-    {
-        case "TOP":
-            player.top = 4;
-            break;
-        case "JUNGLE":
-            player.top = 6;
-            break;
-        case "MIDDLE":
-            player.top = 8;
-            break;
-        case "BOTTOM":
-            player.top = 10;
-            break;
-        case "UTILITY":
-            player.top = 12;
-            break;
-        default:
-            var allPlayerTopValues = new int[] { 4, 6, 8, 10, 12 };
-            var usedTopValues = teamPositions.ContainsKey(player.Team) ? teamPositions[player.Team] : new List<int>();
-            var availableTopValues = allPlayerTopValues.Except(usedTopValues);
-            player.top = availableTopValues.First();
-            break;
-    }
-
-    if (!teamPositions.ContainsKey(player.Team))
-    {
-        teamPositions.Add(player.Team, new List<int>());
-    }
-    teamPositions[player.Team].Add(player.top);
-
-    switch (player.Team)
-    {
-        case "ORDER":
-            player.left = 1;
-            break;
-        case "CHAOS":
-            player.left = 33;
-            break;
-        default:
-            player.left = 1;
-            break;
-    }
-
-    Console.SetCursorPosition(player.left, player.top);
-    Console.Write(player.SummonerName);
-}
-
-foreach (var player in playerList)
-{
-    player.rival = playerList.Find(p => p.top == player.top && p.Team != player.Team);
-}
-
-while (true)
-{
-    foreach (var player in playerList)
-    {
-        var items = await lcuApi.GetItems(player);
-        player.totalGold = items.Sum(item => item.totalGold);
-    }
-
-    var chaos_gold = playerList.Where(p => p.Team == "CHAOS").Sum(p => p.totalGold);
-    var order_gold = playerList.Where(p => p.Team == "ORDER").Sum(p => p.totalGold);
+async Task StartThing()
+{        
+    if (!lcuApi.GetPlayerList(out var playerList)) Exit("No player list found");
 
     foreach (var player in playerList)
     {
-        var left = player.Team == "ORDER" ? 18 : 26;
-        Console.SetCursorPosition(left, player.top);
-        if (player.rival != null)
+        Console.WriteLine($"Player: {player.SummonerName} - {player.Position} - {player.Team}");
+    }
+
+    Console.SetCursorPosition(1, 1);
+    Console.Write("ORDER");
+
+    Console.SetCursorPosition(33, 1);
+    Console.Write("CHAOS");
+    Dictionary<string, List<int>> teamPositions = new Dictionary<string, List<int>>();
+    foreach (var player in playerList)
+    {
+        switch (player.Position)
         {
-            Console.ForegroundColor = player.totalGold > player.rival.totalGold ? ConsoleColor.Green : ConsoleColor.Red;
-            Console.ForegroundColor = player.totalGold == player.rival.totalGold ? ConsoleColor.Yellow : Console.ForegroundColor;
+            case "TOP":
+                player.top = 4;
+                break;
+            case "JUNGLE":
+                player.top = 6;
+                break;
+            case "MIDDLE":
+                player.top = 8;
+                break;
+            case "BOTTOM":
+                player.top = 10;
+                break;
+            case "UTILITY":
+                player.top = 12;
+                break;
+            default:
+                var allPlayerTopValues = new int[] { 4, 6, 8, 10, 12 };
+                var usedTopValues = teamPositions.ContainsKey(player.Team) ? teamPositions[player.Team] : new List<int>();
+                var availableTopValues = allPlayerTopValues.Except(usedTopValues);
+                player.top = availableTopValues.First();
+                break;
         }
-        Console.Write(player.totalGold);
-        Console.ForegroundColor = ConsoleColor.White;
+
+        if (!teamPositions.ContainsKey(player.Team))
+        {
+            teamPositions.Add(player.Team, new List<int>());
+        }
+        teamPositions[player.Team].Add(player.top);
+
+        switch (player.Team)
+        {
+            case "ORDER":
+                player.left = 1;
+                break;
+            case "CHAOS":
+                player.left = 33;
+                break;
+            default:
+                player.left = 1;
+                break;
+        }
+
+        Console.SetCursorPosition(player.left, player.top);
+        Console.Write(player.SummonerName);
     }
 
-    Console.SetCursorPosition(18, 1);
-    Console.ForegroundColor = order_gold > chaos_gold ? ConsoleColor.Green : ConsoleColor.Red;
-    Console.ForegroundColor = order_gold == chaos_gold ? ConsoleColor.Yellow : Console.ForegroundColor;
-    Console.Write(order_gold);
+    foreach (var player in playerList)
+    {
+        player.rival = playerList.Find(p => p.top == player.top && p.Team != player.Team);
+    }
 
-    Console.SetCursorPosition(26, 1);
-    Console.ForegroundColor = chaos_gold > order_gold ? ConsoleColor.Green : ConsoleColor.Red;
-    Console.ForegroundColor = chaos_gold == order_gold ? ConsoleColor.Yellow : Console.ForegroundColor;
-    Console.Write(chaos_gold);
+    while (true)
+    {
+        foreach (var player in playerList)
+        {
+            if(!lcuApi.GetItems(player, out var items)) Exit("No items found");
+            player.totalGold = items.Sum(item => item.totalGold);
+        }
 
-    Console.ForegroundColor = ConsoleColor.White;
+        var chaos_gold = playerList.Where(p => p.Team == "CHAOS").Sum(p => p.totalGold);
+        var order_gold = playerList.Where(p => p.Team == "ORDER").Sum(p => p.totalGold);
 
-    await Task.Delay(1000);
+        foreach (var player in playerList)
+        {
+            var left = player.Team == "ORDER" ? 18 : 26;
+            Console.SetCursorPosition(left, player.top);
+            if (player.rival != null)
+            {
+                Console.ForegroundColor = player.totalGold > player.rival.totalGold ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.ForegroundColor = player.totalGold == player.rival.totalGold ? ConsoleColor.Yellow : Console.ForegroundColor;
+            }
+            Console.Write(player.totalGold);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        Console.SetCursorPosition(18, 1);
+        Console.ForegroundColor = order_gold > chaos_gold ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.ForegroundColor = order_gold == chaos_gold ? ConsoleColor.Yellow : Console.ForegroundColor;
+        Console.Write(order_gold);
+
+        Console.SetCursorPosition(26, 1);
+        Console.ForegroundColor = chaos_gold > order_gold ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.ForegroundColor = chaos_gold == order_gold ? ConsoleColor.Yellow : Console.ForegroundColor;
+        Console.Write(chaos_gold);
+
+        Console.ForegroundColor = ConsoleColor.White;
+
+        await Task.Delay(1000);
+    }
 }
